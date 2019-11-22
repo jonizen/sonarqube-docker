892ed9bf73b37a6c0798a14016d1ad79802a76fe
jonathan.stendahl <jonathan.stendahl@tui.se>
2019-11-22 13:22:03 +0100

    Add first draft

---
+++ Analyze-All.ps1
@@ -0,0 +1,50 @@
+Param(
+	[string] $projectRoot = ("c:\projects\labs"),
+	[string[]] $exclude = ("")
+)
+
+$initpath = get-location
+
+
+#Analyze all projects in ROOT 
+#Put names of folder here
+$projects = @(
+	"SonarCloudDemo"
+);
+
+Foreach ($project in $projects) {
+	$projectShortName = $project.split('\')[0];
+	if ($exclude.Contains($projectShortName)) {
+		Write-Host "Excluding project $projectShortName" -ForegroundColor Yellow
+	}
+	else {
+		$build_path = "$projectRoot\$project"
+		if (Test-Path $build_path) {
+	  		Set-Location $build_path
+		}
+		else {
+			Write-Host "Could not find the path to analyze the project!" -ForegroundColor Red
+		}
+
+		$git_version = (git describe --tags)
+	  
+		$solutionFile = (Get-ChildItem -Path $build_path\ -Filter *.sln -Recurse -File -Name).Where( { $_ -gt 1 }, 'First').Count -gt 0
+		$packageJson = "$build_path\package.json"
+
+		Write-Host "path is: $build_path"
+	  
+		if (!(Test-Path $packageJson) -and ($solutionFile)) {
+			dotnet sonarscanner begin /k:"$project" /v:"$git_version" /d:sonar.cs.opencover.reportsPaths="$build_path\coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs"
+			dotnet build
+			dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput="$build_path\coverage.opencover.xml"
+			dotnet sonarscanner end
+		}
+		else {
+			yarn install
+			yarn run sonar
+		}
+		  
+		Write-Host "All specified services that was found has been built!" -ForegroundColor Green
+		Set-Location $initpath
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# sonarqube-docker
-SonarQube in docker for a clean easy setup
+SonarQube With docker and postgres
+
+
+1. Install runner with core - run this: dotnet tool install --global dotnet-sonarscanner --version 4.X.X take latest
+2. Start docker and login with admin
+3. Go to security and create a token for analysis job.
+4. Copy the token and replace it in the sonar-scanner.properties file.
+5. put sonar-scanner.properties in C:\Users\username\.dotnet\tools\.store\dotnet-sonarscanner\{version}\dotnet-sonarscanner\{version}\tools\netcoreapp2.1\any\sonar-scanner-x.x.x.x.x\conf folder
+6. Update the Analyze-All.ps1 with path to project folder and projects you want to build
+7. Run Analyze-All.ps1 
+
+ref: https://docs.sonarqube.org/latest/analysis/scan/sonarscanner-for-msbuild/
